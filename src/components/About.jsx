73a6bf2e24e3ef84981cefa6336b90de1a620a46
How What <charlau30@gmail.com>
2022-09-14 19:06:40 +0200

    feat: better seo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,17 +16,17 @@ const About = () => {
             <div>
                 <p className='text-4xl lg:text-5xl xl:text-6xl font-medium font-clash mb-8'>&#060; Projets /&#062;</p>
                 <div className='mx-4'>
-                    <ol className="relative border-l border-gray-200 dark:border-gray-700">
+                    <ol className="relative border-l border-gray-200">
                         <li className="mb-10 ml-6 px-4">
-                            <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-purple-400 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-purple-400 rounded-full ring-8 ring-white">
                                 <HiCheck />
                             </span>
-                            <h3 className="flex items-center mb-1 text-lg lg:text-xl font-clash font-medium text-purple-500">Application Keeper <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Plus récent</span></h3>
+                            <h3 className="flex items-center mb-1 text-lg lg:text-xl font-clash font-medium text-purple-500">Application Keeper <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ml-3">Plus récent</span></h3>
                             <time className="block mb-2 text-sm font-normal leading-none text-slate-300">Août 2022</time>
                             <p className="mb-4 text-base lg:text-lg font-normal text-slate-100">Développement de l'application propriétaire Air Rental en React Native.</p>
                         </li>
                         <li className="mb-10 ml-6 px-4">
-                            <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-purple-400 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-purple-400 rounded-full ring-8 ring-white">
                                 <HiCheck />
                             </span>
                             <h3 className="mb-1 text-lg lg:text-xl font-medium font-clash text-purple-500">Composant Calendrier Adalo</h3>
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -6,7 +6,7 @@ const Service = ({ title, text, img, color, id }) => {
         <div className='flex flex-col py-8 md:py-16 xl:py-48 px-8 lg:px-12 xl:px-16'>
             <p className={['font-clash text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-medium mb-8 ', isEven && ' text-right']}>{title}</p>
             <div className={['flex flex-col lg:flex-row justify-between ', isEven && ' lg:flex-row-reverse text-right']}>
-                <img src={img} className='lg:w-1/2' />
+                <img src={img} alt='illustration' className='lg:w-1/2' />
                 <div className={['flex flex-col justify-between ', isEven ? ' items-start' : ' items-end']}>
                     <p className='font-clash text-9xl lg:text-10xl xl:text-230 font-bold text-transparent' style={{ WebkitTextStrokeColor: color, WebkitTextStrokeWidth: '0.2rem' }}>0{id}</p>
                     <p className={['text-sm md:text-lg lg:text-xl font-medium ', isEven ? ' lg:pr-8' : ' lg:pl-8']}>{text}</p>
